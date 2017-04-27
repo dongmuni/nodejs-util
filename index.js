43b92d85d51fdd05b36346df62f52fb5e00bdbb1
dmlim <dmlim@rankwave.net>
2017-04-27 14:24:00 +0900

    Merge modules to index.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// jshint esversion: 6
 /**
  * http://usejsdoc.org/
  */
+
+'use strict';
+
+module.exports = {
+		common: require('./common'),
+		stream: require('./stream'),
+		http: require('./http')
+};
